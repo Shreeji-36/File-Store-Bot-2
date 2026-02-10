25a188fad69c879938d434fe81829cd931f5dd4f
Shreeji-36 <trivediss158@gmail.com>
2026-02-10 14:49:38 +0530

    Update texts.py

--- plugins/texts.py
+++ plugins/texts.py
@@ -17,7 +17,7 @@ async def texts(client, query):
 **Force Sub Message:**
 <pre>{client.messages.get('FSUB', '"<b>◈ Hᴇʏ  {update.effective_user.mention_html()} ×</b>\n"
  <blockquote expandable><b>›› Yᴏᴜ ʜᴀᴠᴇɴ'ᴛ ᴊᴏɪɴᴇᴅ ᴏᴜʀ ᴄʜᴀɴɴᴇʟs ʏᴇᴛ. Pʟᴇᴀsᴇ ᴊᴏɪɴ ᴛʜᴇ ᴄʜᴀɴɴᴇʟs ᴘʀᴏᴠɪᴅᴇᴅ ʙᴇʟᴏᴡ, ᴛʜᴇɴ ᴛʀʏ ᴀɢᴀɪɴ.. !</b></blockquote>\n\n
- <b>››❗Fᴀᴄɪɴɢ ᴘʀᴏʙʟᴇᴍs, ᴄᴏɴᴛᴀᴄᴛ ʜᴇʀᴇ : @Prince_Vegeta_36</b>')}</pre>
+ <b>››❗Fᴀᴄɪɴɢ ᴘʀᴏʙʟᴇᴍs, ᴄᴏɴᴛᴀᴄᴛ ʜᴇʀᴇ : @Prince_Vegeta_36</b>"')}</pre>
 **About Message:**
 <pre>{client.messages.get('ABOUT', 'Empty')}</pre>
 **Reply Message:**
